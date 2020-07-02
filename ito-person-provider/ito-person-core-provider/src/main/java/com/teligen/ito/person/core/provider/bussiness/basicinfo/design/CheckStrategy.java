4892aa47668c997ad2b5a77dda33beec818c8b74
zhangchuan1210 <zhangchuan1210@outlook.com>
2020-07-02 21:50:19 +0800

    提交代码

--- ito-person-provider/ito-person-core-provider/src/main/java/com/teligen/ito/person/core/provider/bussiness/basicinfo/design/CheckStrategy.java
+++ ito-person-provider/ito-person-core-provider/src/main/java/com/teligen/ito/person/core/provider/bussiness/basicinfo/design/CheckStrategy.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 public interface CheckStrategy {
     MatchInfoVo<PersonInfoVo> unique(PersonInfoVo infoVo, List<PersonInfoVo> sourceList);
+
     MatchInfoVo<PersonInfoVo> unique(List<PersonInfoVo> infoVo, List<PersonInfoVo> sourceList);
 
 }
